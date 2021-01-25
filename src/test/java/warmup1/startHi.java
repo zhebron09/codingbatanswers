c758011d382de3b1961467e18998699e8f9df09f
ZOEY <zhebron09@gmail.com>
2021-01-25 12:54:08 -0500

    added solitions to problems

--- src/test/java/warmup1/startHi.java
+++ src/test/java/warmup1/startHi.java
@@ -2,16 +2,24 @@ package warmup1;
 
 public class startHi {
 
+    /*
+
+    Given a string, return true if the string starts with "hi" and false otherwise.
+
+    startHi("hi there") → true
+    startHi("hi") → true
+    startHi("hello hi") → false
+
+     */
+
     public static boolean startHi(String str) {
-        if(str.length() < 2)
-            return false;
-        return(str.substring(0,2).equals("hi"));
 
-
+        if(str.length() < 2)                        // if length of str is less than 2
+            return false;                           // return false(since hi takes 2 indexes it's not possible)
+        return(str.substring(0,2).equals("hi"));    // otherwise if substring of str at index 0 to 2 equals "hi" return true
     }
 
-
     public static void main(String[] args) {
-        System.out.println(startHi("hi there"));
+        System.out.println(startHi("hi there"));    // practice :)
     }
 }
--- src/test/java/warmup1/startOz.java
+++ src/test/java/warmup1/startOz.java
@@ -2,21 +2,33 @@ package warmup1;
 
 public class startOz {
 
+    /*
+
+    Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o'
+    and include the second only if it is 'z', so "ozymandias" yields "oz".
+
+    startOz("ozymandias") → "oz"
+    startOz("bzoo") → "z"
+    startOz("oxx") → "o"
+
+     */
+
     public static String startOz(String str) {
-        String result = "";
 
+        String result = "";                             // create empty String variable
 
-        if(str.length() >=1 && str.charAt(0) == 'o') {
-            result = result + str.charAt(0); }
+        if(str.length() >= 1 && str.charAt(0) == 'o') {  // if length of str is greater than or equal to 1 AND char at index 0 is 'o'
+            result = result + str.charAt(0); }          // result equals result(empty)
 
-        if(str.length() >=2 && str.charAt(1) == 'z') {
-            result = result + str.charAt(1);
+        if(str.length() >= 2 && str.charAt(1) == 'z') {  // if length of str is greater than or equal to 2 AND char at index 1 equals 'z'
+            result = result + str.charAt(1);             // result( which is now 'o' if char at index 0 is 'o' from previous if statement)
+                                                         // equals result plus char at index 1, here we are updating result variable conditions again
         }
-        return result;
+        return result;                                    // return the result after being updated from both if statements
     }
 
 
     public static void main(String[] args) {
-        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("ozymandias"));      // practice :)
     }
 }
