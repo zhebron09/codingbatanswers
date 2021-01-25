1aafaee969691b612e8f8cd0b1c14d85184772db
ZOEY <zhebron09@gmail.com>
2021-01-25 12:59:58 -0500

    updated answer breakdown

--- src/test/java/warmup1/stringE.java
+++ src/test/java/warmup1/stringE.java
@@ -4,24 +4,34 @@ public class stringE {
 
     public static boolean stringE(String str) {
 
-        int result = 0;
-        boolean answer = false;
+        /*
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'e') {
-                result++;
+    Return true if the given string contains between 1 and 3 'e' chars.
+
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false
+
+         */
+
+        int result = 0;                             // create empty int variable
+        boolean answer = false;                     // create boolean variable default set to false
+
+        for (int i = 0; i < str.length(); i++) {    // start for loop where i stats at 0, and goes through the length of the str, and increments by 1
+            if (str.charAt(i) == 'e') {             // if the char of str in the index of i is equal to 'e'
+                result++;                           // result is going to add 1...this is your counter each time there is an e detected it adds 1
             }
-            if(result >= 1 && result <= 3){
-                answer = true;
-            } else answer = false;
+            if(result >= 1 && result <= 3){         // if the result is greater than or equal to 1 and less than or equal to 3
+                answer = true;                      // answer equals true
+            } else answer = false;                  // otherwise the answer is false
         }
-        return answer;
+        return answer;                              // return answer
     }
 
 
         public static void main (String[]args){
 
-            System.out.println(stringE("Hello"));
+            System.out.println(stringE("Hello"));       // practice :)
         }
     }
 
