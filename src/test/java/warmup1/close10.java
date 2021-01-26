265f6b070b4caa727f9b108213d965e8b8eba9f7
ZOEY <zhebron09@gmail.com>
2021-01-26 00:57:41 -0500

    added more notes to warm up1

--- src/test/java/warmup1/close10.java
+++ src/test/java/warmup1/close10.java
@@ -4,18 +4,20 @@ public class close10 {
 
     public static int close10(int a, int b) {
 
-        int aDiff = Math.abs(10-a);
-        int bDiff = Math.abs(10-b);
+        // Math.abs() - this takes the absolute value of a number meaning -2 is 2 and 4 is 4
 
-        if(aDiff < bDiff) {
+        int aDiff = Math.abs(10-a);     // assigning the absolute difference of 10 - a to a variable
+        int bDiff = Math.abs(10-b);     // assigning the absolute difference of 10 - b to a variable
+
+        if(aDiff < bDiff) {             // we are saying here that is aDiff is smaller than bDiff then the answer is a b.c.
+                                        // the higher the number the farther away from 10 it is and a is closer to 10
             return a;
         }
-        if(aDiff > bDiff) {
+        if(aDiff > bDiff) {             // here we are repeating the statement but saying if aDiff is larger than b must be closer to 10
             return b;
         }
-        else { return 0;}
-
-
+        else {return 0;}               // here we are saying if neither aDiff is smaller or larger(the only solution left is that it must
+                                        // be equal then we must return 0
     }
 
     public static void main(String[] args) {
--- src/test/java/warmup1/delDel.java
+++ src/test/java/warmup1/delDel.java
@@ -3,16 +3,17 @@ package warmup1;
 public class delDel {
 
     public static String delDel(String str) {
-        if(str.length() >= 4 && str.substring(1,4).equals("del")){
-            return str.substring(0,1) + str.substring(4);
-        } else {
-            return str;
+
+        if(str.length() >= 4 && str.substring(1,4).equals("del")){   // if the length of the str is greater than or equal to 4( b.c. "del" is 3 chars and starts at index 1)
+                                                                     // so it takes up 4 indexes AND substring starting at index 1 to 4 is equal to "del"
+            return str.substring(0,1) + str.substring(4);            // return substring(0,1) which is the first char and add that to substring(4) which will start
+             } else {                                                // at position 4 and go through the length of the string which omits the "del"
+            return str;                                              // other wise we are just returning the str in it's original format
         }
     }
 
-
     public static void main(String[] args) {
-        System.out.println(delDel("adelbc"));
+        System.out.println(delDel("adelbc"));         // practice :)
     }
 }
 
