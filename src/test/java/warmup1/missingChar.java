4c64c225d242b4d923e7ff8174c626b02c707f4e
ZOEY <zhebron09@gmail.com>
2021-01-28 00:13:06 -0500

    updated problem solution

--- src/test/java/warmup1/missingChar.java
+++ src/test/java/warmup1/missingChar.java
@@ -1,13 +1,26 @@
 package warmup1;
 
 public class missingChar {
+
+    /*
+
+    Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value
+     of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+    missingChar("kitten", 1) → "ktten"
+    missingChar("kitten", 0) → "itten"
+    missingChar("kitten", 4) → "kittn"
+
+     */
+
     public static String missingChar(String str, int n) {
-        return str.substring(0,n)+str.substring(n+1);
-    }
+
+        return str.substring(0,n)+str.substring(n+1);   // return substring of str starting at index 0 and ending at n
+    }                                                   // plus(concat) substring of str starting at index n + 1 (this omits the char of n)
 
 
     public static void main(String[] args) {
-        System.out.println(missingChar("kitten", 1));
+        System.out.println(missingChar("kitten", 1));   // practice :)
     }
 }
 
