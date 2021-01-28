b6389758e275597ba65fc28b4781f6804b45e1f7
ZOEY <zhebron09@gmail.com>
2021-01-27 23:42:22 -0500

    added 3 more answer breakdowns to problems

--- src/test/java/warmup1/sleepIn.java
+++ src/test/java/warmup1/sleepIn.java
@@ -1,15 +1,28 @@
 package warmup1;
 
 public class sleepIn {
+
+    /*
+
+    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+    We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+    sleepIn(false, false) → true
+    sleepIn(true, false) → false
+    sleepIn(false, true) → true
+
+     */
+
     public static boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation) {
-            return true;
+
+        if (!weekday || vacation) {     // here we are saying if it's not a weekday or we are on vacation
+            return true;                // return true
         } else {
-            return false;
+            return false;               // other wise if it is a weekday or we are not on vacation return false
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(sleepIn(false, true));
+        System.out.println(sleepIn(false, true));       // practice
     }
 }
--- src/test/java/warmup1/sumDouble.java
+++ src/test/java/warmup1/sumDouble.java
@@ -1,15 +1,29 @@
 package warmup1;
 
 public class sumDouble {
-    public static int sumDouble(int a, int b) {
-        int sum = a + b;
-        if(a != b)
-            return sum;
 
-        return  2 * sum;
+    /*
+
+    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+
+     */
+
+    public static int sumDouble(int a, int b) {
+
+        int sum = a + b;        // stored the product of a + b into int variable sum
+
+        if(a != b)              // if a is not equal to b
+
+            return sum;         // return sum as is
+
+        return  2 * sum;        // otherwise return the sum * 2
     }
 
     public static void main(String[] args) {
-        System.out.println(sumDouble(3,2));
+        System.out.println(sumDouble(3,2));     // practice
     }
 }
