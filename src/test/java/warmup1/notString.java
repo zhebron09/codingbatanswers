33c2da64b7a99e64bd25d3c01f15c5c012371fc9
ZOEY <zhebron09@gmail.com>
2021-01-29 00:08:38 -0500

    updated solution explanation for 2 more problems

--- src/test/java/warmup1/notString.java
+++ src/test/java/warmup1/notString.java
@@ -1,14 +1,26 @@
 package warmup1;
 
 public class notString {
+
+    /*
+
+    Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+    notString("candy") → "not candy"
+    notString("x") → "not x"
+    notString("not bad") → "not bad"
+
+     */
+
         public static String notString(String str) {
-            if(str.startsWith("not"))
-                return str;
-            return "not " + str;
+
+            if(str.startsWith("not"))       // if str starts with "not" (here we are using a helper method. We can also use substring here)
+                return str;                 // return as is
+            return "not " + str;            // else wise return "not" plus(concat) the original string
         }
 
     public static void main(String[] args) {
         System.out.println(notString("candy"));
-    }
+    }       // practice
 }
 
--- src/test/java/warmup1/posNeg.java
+++ src/test/java/warmup1/posNeg.java
@@ -1,17 +1,30 @@
 package warmup1;
 
 public class posNeg {
+
+    /*
+
+    Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true,
+    then return true only if both are negative.
+
+    posNeg(1, -1, false) → true
+    posNeg(-1, 1, false) → true
+    posNeg(-4, -5, true) → true
+
+     */
+
     public static boolean posNeg(int a, int b, boolean negative) {
-        if(negative) {
-            return (a < 0 && b < 0);
+
+        if(negative) {                                      // if negative is true
+            return (a < 0 && b < 0);                        // return true if a less than 0 AND b < 0 (meaning negative ints)
         }  {
-            return (a < 0 && b >= 0 || a >= 0 && b < 0);
-        }
+            return (a < 0 && b >= 0 || a >= 0 && b < 0);    // otherwise reutn false if a is less than 0 AND b is greater than or equal to 0
+        }                                                   // OR a is greater than or equal to 0 AND b is less than 0 ( if only a or b is negative)
     }
 
     public static void main(String[] args) {
         System.out.println(posNeg(1,-1,false));
-    }
+    }       // practice :)
 }
 
 
